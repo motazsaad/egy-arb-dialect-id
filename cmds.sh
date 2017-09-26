58e6642dc8689915b89af3386539fcfd24f926b3
Motaz Saad <motaz.saad@gmail.com>
2017-09-26 18:26:54 +0300

    add data preparation files

---
+++ cmds.sh
@@ -0,0 +1,8 @@
+
+
+ls -d train/wiki/arz/ | tail -n 1000 | xargs mv -t test/
+
+ls -d train/wiki/ar/ | tail -n 1000 | xargs mv -t test/
+
+
+mv train/wiki/arz/doc0000
--- prepare_data.py
+++ prepare_data.py
@@ -1,10 +1,27 @@
 import glob
+from bs4 import BeautifulSoup
+import os
 
 ar_dir = 'ar-arz/20012017/ar/'
 arz_dir = 'ar-arz/20012017/arz/'
 
-ar_files = glob.glob(ar_dir + "/*.txt")
-arz_files = glob.glob(arz_dir + "/*.txt")
+ar_target_dir = 'train/wiki/ar/'
+arz_target_dir = 'train/wiki/arz/'
 
-for f in ar_files:
-    doc = open(f).read()
+
+def process(src_dir, target_dir):
+    files = glob.glob(src_dir + "/*.txt")
+    for f in files:
+        print('processing {}'.format(f))
+        doc = open(f).read()
+        soup = BeautifulSoup(doc, 'html.parser')
+        text = soup.get_text()
+        filename = os.path.basename(f)
+        outfile = target_dir + filename
+        print('writing out file {}'.format(outfile))
+        with open(outfile, mode='w') as file_writer:
+            file_writer.write(text)
+
+
+process(arz_dir, arz_target_dir)
+process(ar_dir, ar_target_dir)
