64ff717d774e344593b3a3a880ae99c692710c48
Motaz Saad <motaz.saad@gmail.com>
2017-09-27 07:01:02 +0300

    add cmds for preparing and counting dataset

--- cmds.sh
+++ cmds.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 
 # clean train and test
@@ -13,3 +14,11 @@ mv ar_arz_wiki_train/wiki/arz/doc_009* ar_arz_wiki_test/arz/
 mv ar_arz_wiki_train/wiki/ar/doc_009* ar_arz_wiki_test/ar/
 mv ar_arz_wiki_train/wiki/arz/doc_010* ar_arz_wiki_test/arz/
 mv ar_arz_wiki_train/wiki/ar/doc_010* ar_arz_wiki_test/ar/
+
+
+
+# count train and test
+ls ar_arz_wiki_train/wiki/arz/ | wc -l
+ls ar_arz_wiki_train/wiki/ar/ | wc -l
+ls ar_arz_wiki_test/arz/ | wc -l
+ls ar_arz_wiki_test/ar/ | wc -l
